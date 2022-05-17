08e9dd6d48f9756cdf8df7c22d01141626ce9d1e
RHenrique22 <72585927+RHenrique22@users.noreply.github.com>
2022-05-16 22:10:31 -0300

    v1

--- src/main/java/br/edu/ifpb/padroes/realstatev2/payment/PaymentService.java
+++ src/main/java/br/edu/ifpb/padroes/realstatev2/payment/PaymentService.java
@@ -5,22 +5,29 @@ import br.edu.ifpb.padroes.realstatev2.payment.processors.GovernmentTaxesPayment
 import br.edu.ifpb.padroes.realstatev2.payment.processors.PropertyPayment;
 import br.edu.ifpb.padroes.realstatev2.payment.processors.RealEstatePayment;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class PaymentService {
 
     private final GovernmentTaxesPayment governmentTaxesPayment;
-    private final RealEstatePayment realEstatePayment;
-    private final PropertyPayment propertyPayment;
+    private final RealEstatePayment      realEstatePayment;
+    private final PropertyPayment        propertyPayment;
+    private       PaymentProcessor       process;
 
     public void pay(Property sale) {
+        this.governmentTaxesPayment.next(realEstatePayment)
+                                   .next(propertyPayment);
+
+        this.governmentTaxesPayment.process(sale);
 
         // TODO - implementar Chain of Responsibility para que ordem dos métodos de pagamento seja dinâmica (definida em tempo de execução)
-        governmentTaxesPayment.process(sale);
+        /*governmentTaxesPayment.process(sale);
         realEstatePayment.process(sale);
-        propertyPayment.process(sale);
+        propertyPayment.process(sale);*/
 
     }
 
